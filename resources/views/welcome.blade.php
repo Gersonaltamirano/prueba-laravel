fc044dd43f2597a214d891f830eccd79b4e76209
JulioReyes15 <julioreyes1520@gmail.com>
2022-05-13 14:15:38 -0600

    pruebas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Julio Reyes</title>
+        <title>Julio Reyes2</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
